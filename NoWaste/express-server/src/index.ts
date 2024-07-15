a1c377156c07cbcbbb2a33a00697336b384f3d6c
Nicolas Dartayeta <63559546+nicolasdartayeta@users.noreply.github.com>
2024-07-15 18:35:01 -0300

    base del compose

--- NoWaste/express-server/src/index.ts
+++ NoWaste/express-server/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import express from 'express';
+
+const app = express();
+ 
+app.get('/', async (req, res) => {
+  res.json({ hello: "world" });
+});
+ 
+const port = Number(process.env.PORT);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
